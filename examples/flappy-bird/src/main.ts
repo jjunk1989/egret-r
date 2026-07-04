3a967287aff86fd81be32c26cdde4f7873662d66
fcx <fcx@163.com>
2026-07-04 18:17:48 +0800

    debug: add eui import, darker bg, black text

--- examples/flappy-bird/src/main.ts
+++ examples/flappy-bird/src/main.ts
@@ -1,4 +1,5 @@
 import { egret } from '@egret-r/core';
+import '@egret-r/eui';
 import '@egret-r/game';
 
 class Main extends egret.DisplayObjectContainer {
@@ -7,7 +8,7 @@ class Main extends egret.DisplayObjectContainer {
     this.addEventListener(egret.Event.ADDED_TO_STAGE, () => {
       console.log('Main added to stage');
       const sky = new egret.Shape();
-      sky.graphics.beginFill(0x87ceeb);
+      sky.graphics.beginFill(0x0ea5e9);
       sky.graphics.drawRect(0, 0, 480, 700);
       sky.graphics.endFill();
       this.addChild(sky);
@@ -15,8 +16,9 @@ class Main extends egret.DisplayObjectContainer {
       const label = new egret.TextField();
       label.text = 'Hello Egret!';
       label.size = 32;
-      label.textColor = 0xffffff;
+      label.textColor = 0x000000;
       label.x = 100; label.y = 300;
+      label.width = 280;
       this.addChild(label);
     }, this);
   }
